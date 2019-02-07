042908ca47b177b53d229f601abbc1ebd00aa9c3
JessC Jess <jessicacornejojara@gmail.com>
2019-02-06 23:40:53 -0800

    Initial commit, vue project with vue-router & scss

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
 import Vue from 'vue'
-import App from './App.vue'
-
+import App from './App'
+import router from './router'
 Vue.config.productionTip = false
-
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+router,
+render: createEl => createEl(App)
+}).$mount('#app');
